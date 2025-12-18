4a43296e9771e16db895dc172ba7d7d33b526d9f
winsam003 <winsam003@gmail.com>
2025-12-18 21:34:22 +0900

    feat: 애드센스 코드 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ScrollButtons from "@/components/custom-ui/ScrollButtons";
 import Image from "next/image";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "나만의 만능 툴 박스 | 무료 온라인 도구",
@@ -15,6 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8286025705631064"
+          crossOrigin="anonymous"
+          strategy="afterInteractive" // 페이지가 로드된 후 부드럽게 광고 스크립트 실행
+        />
+      </head>
       <body className="antialiased bg-slate-50">
         <header className="p-4 border-b bg-white font-bold flex items-center justify-center gap-2">
           {/* 2. 로고 이미지 추가 */}
