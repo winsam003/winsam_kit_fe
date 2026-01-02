7bc4392e934270533d2778ea9b8a9848ea6f4fe0
winsam003 <winsam003@gmail.com>
2026-01-02 14:56:10 +0900

    sitemap 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://winsam.xyz", // 내 사이트 주소
+  generateRobotsTxt: true, // robots.txt 파일도 같이 생성
+  sitemapSize: 7000,
+};
